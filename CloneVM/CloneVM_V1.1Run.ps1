f50997aa122dae389ce95714bed712f0014f2463
vishwanathsrikanth <vishwanath.srikanth@gmail.com>
2014-06-17 10:33:51 +0530

    added run command

---
+++ CloneVM/CloneVM_V1.1Run.ps1
@@ -0,0 +1,9 @@
+﻿C:\Users\vishwanath\Documents\GitHub\AzurePowerShell\CloneVM\CloneVM_V1.1.ps1 `
+ -sourceVMName 'uatazureinsight' `
+ -sourceCloudServiceName 'uatazureinsight' `
+ -targetVMName 'tneazureinsight' `
+ -targetCloudServiceName 'tneduazureinsight' `
+ -targetStorageAccountName 'azureinsighteastus' `
+ -targetlocation 'East US'`
+ -subscriptionName "Hemant - HANU" `
+ -publishSettingsFilePath "D:\PublishSettingsStore\Hemant - HANU-3-6-2014-credentials.publishsettings"
